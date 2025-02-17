a7fe44f5d7823068e66c91d0fc3899cf131d14be
Patryk Wiśniewski <patrix91@users.noreply.github.com>
2025-02-17 13:02:21 +0100

    Utworzenie pliku konfiguracyjnego dla Procesu Odpluskwiania; Utworzenie kontrolera BaseApiController; Utworzenie metody LogIn dla AccountController; Utworzenie LoginDto;

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,27 +1,53 @@
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+using API.Entities;
 using System.Text;
 using API.Data;
-using API.Entities;
-using Microsoft.AspNetCore.Mvc;
+using API.DTO;
 
 namespace API.Controllers;
 
 public class AccountController(DataContext dataContext) : BaseApiController
 {
     [HttpPost("register")] // account/register
-    public async Task<ActionResult<AppUser>> Register(string userName, string password){
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    {
+        if(await UserExists(registerDto.Username)) return BadRequest("Uzytkownik o podaj nazwie już istnieje.");
 
         using var hmac = new HMACSHA512();
         var user = new AppUser{
-            UserName = userName,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
 
         dataContext.Users.Add(user);
         await dataContext.SaveChangesAsync();
-        return Ok(dataContext);
+        return user;
     }
 
-    
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await dataContext.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
+    }    
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await dataContext.Users.FirstOrDefaultAsync(x => 
+        x.UserName == loginDto.UserName.ToLower());
+
+        if(user == null) return Unauthorized("Niepoprawna nazwa użytkownika.");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for(int i = 0; i < computedHash.Length; i++)
+        {
+            if  (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Niepoprawne hasło.");
+        }
+        return user;
+    }
 }
---
+++ API/DTO/LoginDto.cs
@@ -0,0 +1,8 @@
+namespace API.DTO;
+
+public class LoginDto
+{
+    public required string UserName { get; set; }
+    public required string Password { get; set; }
+    
+}
